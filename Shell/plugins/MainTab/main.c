4f5cca432deba3645fd341e58a67fcf9e6e30d02
FIL <ftcrck@gmail.com>
2013-03-18 03:31:45 +0400

    [MainTab] убрал действие с GREEN_BUTTON

--- Shell/plugins/MainTab/main.c
+++ Shell/plugins/MainTab/main.c
@@ -26,7 +26,7 @@ void OnRedraw(void)
 	if (cfg_use_rs) DrawText(ws, lgp[lgpSoftkeyRight], GetFont(fontSoftkeys), TEXT_ALIGNRIGHT, GetColor(colorSoftkeys), TEXT_TYPE_SOFTKEY);
 }
 
-void OnKey(int key, int type)
+void OnKey(unsigned int key, unsigned int type)
 {
 	if (type == KEY_DOWN)
 	{
@@ -40,9 +40,6 @@ void OnKey(int key, int type)
 			break;
 			case ENTER_BUTTON:
 				if (cfg_use_enter) ExecShortcut(cfg_enter_func);
-			break;
-			case GREEN_BUTTON:
-				ExecShortcut("REC_DIALED_LIST");
 			break;
 		}
 	}
