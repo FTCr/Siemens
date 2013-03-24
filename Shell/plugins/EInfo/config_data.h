974e1996ee98d119142f06143ec1a1eda5715335
FIL <ftcrck@gmail.com>
2013-03-25 02:49:45 +0400

    [EInfo] дополнительная информация

---
+++ Shell/plugins/EInfo/config_data.h
@@ -0,0 +1,25 @@
+#ifndef _CONFIG_DATA_H_
+	#define _CONFIG_DATA_H_
+
+extern const unsigned int cfg_desk_id;
+
+extern const unsigned int cfg_freeram_apply;
+extern const unsigned int cfg_freeram_update_time;
+extern const unsigned int cfg_freeram_pos_x, cfg_freeram_pos_y;
+/*extern const unsigned int cfg_freeram_align;*/
+extern const char cfg_freeram_string[32];
+extern const unsigned int cfg_freeram_show_in;
+
+extern const unsigned int cfg_cpuload_apply;
+extern const unsigned int cfg_cpuload_update_time;
+extern const unsigned int cfg_cpuload_pos_x, cfg_cpuload_pos_y;
+/*extern const unsigned int cfg_freeram_align;*/
+extern const char cfg_cpuload_string[32];
+
+extern const unsigned int cfg_akkucapa_apply;
+extern const unsigned int cfg_akkucapa_update_time;
+extern const unsigned int cfg_akkucapa_pos_x, cfg_akkucapa_pos_y;
+/*extern const unsigned int cfg_freeram_align;*/
+extern const char cfg_akkucapa_string[32];
+
+#endif
