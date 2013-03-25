f07af4c87887bda5d677dd8495e30bd646d1e803
FIL <ftcrck@gmail.com>
2013-03-26 03:46:41 +0400

    [KeyLocker] обновил конфиг

--- Shell/plugins/KeyLocker/config_data.c
+++ Shell/plugins/KeyLocker/config_data.c
@@ -12,7 +12,7 @@ __CFG_BEGIN(0)
 
 	__root const CFG_HDR cfghdr2 = {CFG_CBOX,"Lock type", 0, 3};
 	__root const unsigned int cfg_type_block = 2;
-	__root const CFG_CBOX_ITEM cfgcbox0[3] = {"KEY_DOWN", "LONG_PRESS", "Double press"};
+	__root const CFG_CBOX_ITEM cfgcbox0[3] = {"Key down", "Long press", "Double press"};
 
 	__root const CFG_HDR cfghdr3 = {CFG_UINT, "Double press waiting interval", 10, 200};
 	__root const unsigned int cfg_wait_time = 65;
