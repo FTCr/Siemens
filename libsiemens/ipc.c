54c1d99e5594e0547e7bbdf2248785d79b6cb524
FIL <ftcrck@gmail.com>
2013-03-25 21:29:31 +0400

    [libsiemens] обновил функцию IPC_SendMessage. Поднял до версии 0.2

--- libsiemens/ipc.c
+++ libsiemens/ipc.c
@@ -1,10 +1,9 @@
 #include <swilib.h>
 
-void IPC_SendMessage(const char *name_to, const char *name_from, const int mess)
+void IPC_SendMessage(IPC_REQ *ipc, const char *name_to, const char *name_from, const int mess, void *data)
 {
-	static IPC_REQ ipc;
-	ipc.name_to   = name_to;
-	ipc.name_from = name_from;
-	ipc.data      = 0;
-	GBS_SendMessage(MMI_CEPID, MSG_IPC, mess, &ipc);
+	ipc->name_to   = name_to;
+	ipc->name_from = name_from;
+	ipc->data      = data;
+	GBS_SendMessage(MMI_CEPID, MSG_IPC, mess, ipc);
 }
--- libsiemens/ipc.h
+++ libsiemens/ipc.h
@@ -1,7 +1,14 @@
 #ifndef _LIBS_IPC_H_
 	#define _LIBS_IPC_H_
 
-void IPC_SendMessage(const char *name_to, const char *name_from, const int mess);
+#define IPC_XTASK_NAME "XTask"
+
+#define IPC_XTASK_IDLE        0x01
+#define IPC_XTASK_SHOW_CSM    0x02
+#define IPC_XTASK_LOCK_SHOW   0x03
+#define IPC_XTASK_UNLOCK_SHOW 0x04
+
+void IPC_SendMessage(IPC_REQ *ipc, const char *name_to, const char *name_from, const int mess, void *data);
 
 	
 #endif
