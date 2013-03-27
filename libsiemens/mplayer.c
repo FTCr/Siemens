562414b4719e5ea7123cee09f2f5351ff44a21c3
FIL <ftcrck@gmail.com>
2013-03-27 04:40:39 +0400

    [libsiemens] переименовал и перенес макросы управления медиаплеера

--- libsiemens/mplayer.c
+++ libsiemens/mplayer.c
@@ -1,20 +1,5 @@
 #include <swilib.h>
-
-#define PLAYER_NEXT    0x01
-#define PLAYER_PREV    0x02
-#define PLAYER_RESTART 0x03
-#define PLAYER_MUTE    0x0A
-#define PLAYER_KILL    0x0C
-#define PLAYER_PLAY    0x0E
-#define PLAYER_STOP    0x0F
-#define PLAYER_PAUSE   0x10
-#define PLAYER_TOGGLE  0x11
-#define PLAYER_FWD     0x12
-#define PLAYER_RWN     0x13
-#define PLAYER_NRM     0x14
-#define PLAYER_VOL_UP  0x15
-#define PLAYER_VOL_DN  0x16
-#define PLAYER_REPEAT1 0x18
+#include "mplayer.h"
 
 void Send_MPlayer_Command(unsigned short cmd, short substract)
 {
@@ -25,18 +10,18 @@ void MPlayer_VolChange(short dir)
 {
 	int cmd;
 	if(dir==1)
-		cmd = PLAYER_VOL_UP;
+		cmd = MPLAYER_VOL_UP;
 	else
-		cmd = PLAYER_VOL_DN;
+		cmd = MPLAYER_VOL_DN;
 	Send_MPlayer_Command(cmd, 2);
 	Send_MPlayer_Command(cmd, 0);
 }
 
-void MPlayer_Stop(void)     {Send_MPlayer_Command(PLAYER_STOP, 0);}
-void MPlayer_Start(void)    {Send_MPlayer_Command(PLAYER_PLAY, 0);}
-void MPlayer_Next(void)     {Send_MPlayer_Command(PLAYER_NEXT, 0);}
-void MPlayer_Prev(void)     {Send_MPlayer_Command(PLAYER_PREV, 0);}
-void MPlayer_Toggle(void)   {Send_MPlayer_Command(PLAYER_TOGGLE, 0);}
-void MPlayer_Shutdown(void) {Send_MPlayer_Command(PLAYER_KILL, 0);}
-void MPlayer_Mute(void)     {Send_MPlayer_Command(PLAYER_MUTE, 0);}
-void MPlayer_Pause(void)    {Send_MPlayer_Command(PLAYER_PAUSE, 0);}
+void MPlayer_Stop(void)     {Send_MPlayer_Command(MPLAYER_STOP, 0);}
+void MPlayer_Start(void)    {Send_MPlayer_Command(MPLAYER_PLAY, 0);}
+void MPlayer_Next(void)     {Send_MPlayer_Command(MPLAYER_NEXT, 0);}
+void MPlayer_Prev(void)     {Send_MPlayer_Command(MPLAYER_PREV, 0);}
+void MPlayer_Toggle(void)   {Send_MPlayer_Command(MPLAYER_TOGGLE, 0);}
+void MPlayer_Shutdown(void) {Send_MPlayer_Command(MPLAYER_KILL, 0);}
+void MPlayer_Mute(void)     {Send_MPlayer_Command(MPLAYER_MUTE, 0);}
+void MPlayer_Pause(void)    {Send_MPlayer_Command(MPLAYER_PAUSE, 0);}
--- libsiemens/mplayer.h
+++ libsiemens/mplayer.h
@@ -1,6 +1,22 @@
 #ifndef _LIBS_MPLAYER_H_
 	#define _LIBS_MPLAYER_H_
 
+#define MPLAYER_NEXT    0x01
+#define MPLAYER_PREV    0x02
+#define MPLAYER_RESTART 0x03
+#define MPLAYER_MUTE    0x0A
+#define MPLAYER_KILL    0x0C
+#define MPLAYER_PLAY    0x0E
+#define MPLAYER_STOP    0x0F
+#define MPLAYER_PAUSE   0x10
+#define MPLAYER_TOGGLE  0x11
+#define MPLAYER_FWD     0x12
+#define MPLAYER_RWN     0x13
+#define MPLAYER_NRM     0x14
+#define MPLAYER_VOL_UP  0x15
+#define MPLAYER_VOL_DN  0x16
+#define MPLAYER_REPEAT1 0x18
+
 void MPlayer_Stop(void);
 void MPlayer_Start(void);
 void MPlayer_Next(void);
