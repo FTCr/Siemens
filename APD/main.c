8435d7307001a1365708a976ccbb8ed63c9391dd
FIL <fil@recursion.pro>
2013-05-19 12:41:53 +0400

    [APD] теперь не зависаем после звонка входящего

--- APD/main.c
+++ APD/main.c
@@ -92,21 +92,21 @@ int maincsm_onmessage(CSM_RAM* data, GBS_MSG* msg)
 
 int IDLEcsm_onmessage(CSM_RAM *icsm, GBS_MSG *msg)
 {
-	static int flag;
+	static unsigned int flag;
 	if ((msg->msg == MSG_STATE_OF_CALL || msg->msg == MSG_INCOMMING_CALL) && msg->submess)
 	{
 		if (APlayer_GetPlayStatus() == APLAYER_PLAY)
 		{
 			flag = 1;
-			APlayer_Pause();
+			APlayer_FakePause();
 		}
 	}
 	if (msg->msg == MSG_END_CALL)
 	{
 		if (flag == 1)
 		{
-			APlayer_Play();
 			flag = 0;
+			APlayer_FakePlay();
 		}
 	}
 	
